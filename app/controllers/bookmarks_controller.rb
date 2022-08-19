619bf8acfddc65ce94c20a0f38fe71cd2862afa7
oonagh corrigan <oonaghcorrigan97@gmail.com>
2022-08-19 16:03:05 +0200

    bundle installed

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,7 @@
+class BookmarksController < ApplicationController
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+    # raise
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  root to: "lists#index"
+  resources :lists, only: [ :index, :show, :new, :create, :destroy ] do
+    resources :bookmarks, only: [ :new, :create ]
+  end
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :movies
 end
